c50ad3e866d10b6b7f1bcd42f542384c871bf605
Vishnu <vishnu.dev.py@gmail.com>
2024-06-19 15:08:36 +0530

    Create a task menu.

---
+++ app.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    while True:
+        print("\nTask Manager Menu:")
+        print("1. Add Task")
+        print("2. Edit Task")
+        print("3. Delete Task")
+        print("4. View All Tasks")
+        print("5. Filter Tasks by Priority")
+        print("6. Exit")
+
+        choice = input("Enter your choice (1-6)")
